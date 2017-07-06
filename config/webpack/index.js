3ffaa24097d6e01f9169c0c924436d2af703d0ec
Artem Prokhatskyi <Artem.Prokhatskyi@worldapp.com>
2017-07-06 20:42:14 +0300

    init project builder

---
+++ config/webpack/index.js
@@ -0,0 +1,12 @@
+/* eslint-disable */
+'use strict';
+
+const dev = require('./Dev');
+const dist = require('./Dist');
+const test = require('./Test');
+
+module.exports = {
+    dev,
+    dist,
+    test
+};
